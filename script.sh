d88061104270dd2fc26bb0b114eda61a82d259a7
AhmadAlsous <ahmadalsous17@gmail.com>
2023-07-08 21:47:20 +0300

    added file filtering

--- script.sh
+++ script.sh
@@ -14,31 +14,50 @@ print_help() {
 	echo "  -h, --help       Display help message"
 }
 
-validate_path(){
-	path=$1
-	while [ ! -d "$path" ]; do
-		echo "Error: Invalid directory path: $path"
-		read -p "Please enter a valid directory path (e.g., /home) " path
+validate_directory(){
+	directory=$1
+	while [ ! -d "$directory" ]; do
+		echo "Error: Invalid directory path: $directory"
+		read -p "Please enter a valid directory path (e.g., /home) " directory
 	done
 }
 
 validate_extension(){
 	extension=$1
 	if ! [[ "$extension" =~ ^\.[A-Za-z0-9]+$ ]]; then
-		echo "Error: Invalid file extension: $1"
+		echo "Error: Invalid file extension: $extension"
 		return 1
 	fi
 	return 0
 }
 
+validate_filter_option(){
+	if [ -z "$1" ]; then
+        echo "Missing argument for option $2"
+        return 1
+    fi
+    if ! [[ "$1" =~ $3 ]]; then
+        echo "Error: Invalid file $4: $1"
+        return 2
+    fi
+    local -n filter=$5
+	filter="$1"
+    return 0
+}
+
+size="-1000G"
+time_minute="+0"
+time_day="-999999"
+permission="000"
+
 while [ $# -gt 0 ]; do
 	case "$1" in
-		-p | --path | -e | --extension)
+		-d | --directory | -e | --extension)
 			if [ -z "$2" ] || [[ "$2" == -* ]]; then
 				echo "Missing argument for option $1"
 		    	shift
-			elif [ "$1" = "-p" ] || [ "$1" = "--path" ]; then
-				validate_path "$2"
+			elif [ "$1" = "-d" ] || [ "$1" = "--directory" ]; then
+				validate_directory "$2"
 				shift 2
 			elif [ "$1" = "-e" ] || [ "$1" = "--extension" ]; then
 				shift
@@ -52,6 +71,34 @@ while [ $# -gt 0 ]; do
 				done
 			fi
 			;;
+		-s | --size)
+			validate_filter_option $2 $1 "^[+-]?[0-9]+[bcwkMG]$" "size" "size"
+			if ! [ $? -eq 1 ]; then
+				shift
+			fi
+			shift
+			;;
+		-tm | --timeminute)
+			validate_filter_option $2 $1 "^[+-]?[0-9]+$" "last modified time" "time_minute"
+			if ! [ $? -eq 1 ]; then
+				shift
+			fi
+			shift
+			;;
+		-td | --timeday)
+			validate_filter_option $2 $1 "^[+-]?[0-9]+$" "last modified time" "time_day"
+            if ! [ $? -eq 1 ]; then
+                shift
+            fi
+            shift
+			;;
+		-p | --permission)
+			validate_filter_option $2 $1 "^([0-7]{3}|(([ugoa]=[rwx],)*[uoga]=[rwx]))$" "permission" "permission"
+			if ! [ $? -eq 1 ]; then
+                shift
+            fi
+            shift
+			;;
 		-h | --help)
 			print_help
 			exit 0
@@ -64,9 +111,9 @@ while [ $# -gt 0 ]; do
 	esac
 done
 
-if [ -z "$path" ]; then
-	read -p "What directory do you want to search? " path
-	validate_path "$path"
+if [ -z "$directory" ]; then
+	read -p "What directory do you want to search? " directory
+	validate_directory "$directory"
 fi
 
 while [ -z "$extensions" ]; do
@@ -88,11 +135,11 @@ for ext in "${extensions[@]}"; do
   regex_pattern+="$ext"
 done
 
-echo "Searching for all files with extension ${extensions[@]} in directory $path"
-output=$(find "$path" -regex ".*\($regex_pattern\)$" -type f -printf "%u %s bytes %M %AF %Ar %p\n" | sort -k2,2n)
+echo "Searching for files with extension ${extensions[@]} in directory $directory"
+output=$(find "$directory" -regex ".*\($regex_pattern\)$" -type f -size "$size" -mmin "$time_minute" -mtime "$time_day" -perm "-$permission" -printf "%u %s bytes %M %AF %Ar %p\n" | sort -k2,2n)
 
 if [ -z "$output" ]; then
-	echo "No files were found in directory $path with extension $extension."
+	echo "No files were found in directory $directory with extension ${extensions[@]}."
 	exit 0
 fi
 
